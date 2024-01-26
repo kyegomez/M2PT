21b34a76dfd8fd2599753a65a3179995241ed496
Kye <kye@apacmediasolutions.com>
2024-01-26 14:19:01 -0500

    [CLEANUP]

--- m2pt/__init__.py
+++ m2pt/__init__.py
@@ -1,7 +1,14 @@
-from m2pt.main import CrossModalReParametrization, MPTransformerBlock
+from m2pt.main import (
+    CrossModalReparamLinear,
+    reparameterize_aux_into_target_model,
+    CrossModalReParametrization,
+    MPTransformerBlock,
+)
 
 
 __all__ = [
+    "CrossModalReparamLinear",
+    "reparameterize_aux_into_target_model",
     "CrossModalReParametrization",
     "MPTransformerBlock",
 ]
